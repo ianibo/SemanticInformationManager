46c2de58bda5920e78ea8e323c2aa1b0c2062b71
Ian Ibbotson <ianibbo@googlemail.com>
2011-06-06 12:10:17 +0100

    Added services

---
+++ SemanticInformationManager/grails-app/services/com/k_int/sim/4StoreWrappperService.groovy
@@ -0,0 +1,15 @@
+package com.k_int.sim
+
+import grails.converters.*
+
+class 4StoreWrappperService {
+
+  static transactional = true
+
+  def issueSparql(sparql) {
+
+    // def jsonArray = JSON.parse("""['foo','bar', { a: 'JSONObject' }]""")
+    // println "Class of jsonArray: ${jsonArray.class.name}"
+    // jsonArray.each { println "Value: ${it}" }
+  }
+}
