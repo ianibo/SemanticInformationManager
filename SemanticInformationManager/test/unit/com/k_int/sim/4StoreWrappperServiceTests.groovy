e31043b59fd2c3594753fe880aeaaf935b6ece22
Ian Ibbotson <ianibbo@googlemail.com>
2011-06-30 18:34:37 +0100

    updated

---
+++ SemanticInformationManager/test/unit/com/k_int/sim/4StoreWrappperServiceTests.groovy
@@ -0,0 +1,17 @@
+package com.k_int.sim
+
+import grails.test.*
+
+class 4StoreWrappperServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
