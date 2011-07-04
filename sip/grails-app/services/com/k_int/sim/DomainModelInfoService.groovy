1350dd1a83bf611a86519b9ea97c6d5176d1a4f4
Ian Ibbotson <ianibbo@googlemail.com>
2011-07-04 13:30:35 +0100

    Added nimble as a plugin dependency, so we can make grants against nimble base users and base roles

--- sip/grails-app/services/com/k_int/sim/DomainModelInfoService.groovy
+++ sip/grails-app/services/com/k_int/sim/DomainModelInfoService.groovy
@@ -2,6 +2,13 @@ package com.k_int.sim
 
 import com.k_int.sip.domain.*
 
+import grails.plugins.nimble.core.LevelPermission
+import grails.plugins.nimble.core.Role
+import grails.plugins.nimble.core.Group
+import grails.plugins.nimble.core.AdminsService
+import grails.plugins.nimble.core.UserService
+
+
 class DomainModelInfoService {
 
     static transactional = true
@@ -14,6 +21,12 @@ class DomainModelInfoService {
       // system defined context for that class. The context provides an access point for search and record creation.
       // Only the system administrator should be granted permission to these contexts.
       // Each system level context should have the URI uri:gorm:--fully-qualified-domain-class/context
+
+      println "Looking up admin user..."
+      def admin_user = grails.plugins.nimble.core.UserBase.findByUsername('admin')
+      println "admin user: ${admin_user}"
+
+      println "Syncing context models with local GORM domain classes...."
 
       def gorm_local_repo = GormSIPRepository.findByUri('uri:gorm:localrepo') ?: new GormSIPRepository(uri:'uri:gorm:localrepo').save()
 
