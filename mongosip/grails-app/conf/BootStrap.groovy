78ff50733905b3ad05642465890e8ac448203862
ianibo <ianibbo@googlemail.com>
2012-03-05 12:25:57 +0000

    Added core schema

--- mongosip/grails-app/conf/BootStrap.groovy
+++ mongosip/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,14 @@
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+  def init = { servletContext ->
+    setup();
+  }
+
+  def destroy = {
+  }
+
+  def setup() {
+    // 1. See if we can locate an object definedby "http://json-schema.org/schema#" and with the id "http://json-schema.org/schema#" in the mongo repository
+    // If not, load it.
+  }
 }
